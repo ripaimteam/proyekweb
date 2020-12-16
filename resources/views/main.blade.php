232a97f6eaf09f37808afe4eccbd74b38a99f0cf
ivanjr22 <ivanjr023@gmail.com>
2020-12-16 19:46:11 +0700

    Add Home Blade

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -182,25 +182,6 @@
 
         @yield('content')
 
-        <div class="breadcrumbs">
-            <div class="col-sm-4">
-                <div class="page-header float-left">
-                    <div class="page-title">
-                        <h1>Dashboard</h1>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-8">
-                <div class="page-header float-right">
-                    <div class="page-title">
-                        <ol class="breadcrumb text-right">
-                            <li><a href="#">Dashboard</a></li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
-
                 </div>
             </div><!-- .animated -->
         </div><!-- .content -->
