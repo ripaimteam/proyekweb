729ebe0638633aa1dcf95ed0aa40d5e0c4a828f5
ivanjr22 <ivanjr023@gmail.com>
2020-12-16 19:38:37 +0700

    Edit Main Blade

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,13 +1,10 @@
 
 <!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Welcome to Bromo Travelling</title>
+    <title>@yield('title') Welcome to Bromo Travelling</title>
     <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -180,6 +177,10 @@
 
         </header><!-- /header -->
         <!-- Header-->
+
+        @yield('breadcrumbs')
+
+        @yield('content')
 
         <div class="breadcrumbs">
             <div class="col-sm-4">
