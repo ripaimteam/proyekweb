d2143d16fdc21755917cf2e0718a509b7bda04f2
15faiq <maulanafaiq85@gmail.com>
2021-01-04 20:32:55 +0700

    Contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -26,5 +26,18 @@
 @endsection
 
 @section('content')
-    
+<hr>
+    <table>
+        <h3>&nbsp;Detail Services</h3>
+        <br>&nbsp;&nbsp;Nama     : Faiq Maulana
+        <br>&nbsp;&nbsp;Whatsapp : 081385772392
+        <br>&nbsp;&nbsp;Nama     : Muhammad Ivan Fadhilah
+        <br>&nbsp;&nbsp;Whatsapp : 089514286426
+        <br>&nbsp;&nbsp;SosMed   : <a href="/contact">Instagram/Facebook/Twitter</a>
+        <br>&nbsp;&nbsp;Domisili : Malang Soekarno Hatta
+    </table>
+    <hr>
+    <h3>&nbsp;&nbsp;My Lokasi</h3>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8870.84212515699!2d112.6153039765334!3d-7.946039262338932!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78827687d272e7%3A0x789ce9a636cd3aa2!2sState%20Polytechnic%20of%20Malang!5e0!3m2!1sen!2sid!4v1602080994981!5m2!1sen!2sid" width="500" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    <hr>  
 @endsection
