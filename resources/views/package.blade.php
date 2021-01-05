c971e1fc2677bffa9d8c1aa49694fe269d23ab6a
ivanjr22 <ivanjr023@gmail.com>
2021-01-05 08:50:17 +0700

    Edit Package

--- resources/views/package.blade.php
+++ resources/views/package.blade.php
@@ -31,202 +31,143 @@
         <div class="page-title">
             <ol class="breadcrumb text-right"></ol>
                 <div class="row"><div class="boxer2">
-<div class="col-md-4"><div class="rental-produk"> 
-<div class="rental-gambar-center">
-<div class="rental-gambar"><a  ><img src="img/per.jpeg" width="220" height="147"  />
-</a>
-</div></div>
-<div class="rental-title-produk"><a>Private Trip 1</a></div>
-<div class="widget biz_hours-widget list">
-                <ul class="unstyled">
+        <div class="col-md-4"><div class="rental-produk"> 
+        <div class="rental-gambar-center">
+        <div class="rental-gambar"><a  ><img src="img/per.jpeg" width="220" height="147"  />
+        </a>
+        </div></div>
+        <div class="rental-title-produk"><a>Private Trip 1</a></div>
+        <div class="widget biz_hours-widget list">
+        <ul class="unstyled">
         								
     <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 1.500.000</strong></span></li>					
     <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
     <li><span>Fasilitas :</span> <span class="right">Non Dokumentasi </strong></span></li>				
     <li><span>Lokasi Jemput :</span> <span class="right">Malang</span></li> 					
     <li><span>Kapasitas :</span> <span class="right">Maks 6 orang</span></li>
- 
-            
-            </ul>
+
+        </ul>
     </div>
 
-<div class="tombol">
-<a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
-</div>
+    <div class="tombol">
+    <a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
+    </div>
+    </div>
+    </div>
 
-</div></div>
-
-<div class="col-md-4"><div class="rental-produk"> 
-<div class="rental-gambar-center">
-<div class="rental-gambar"><a><img src="img/wer.jfif"  width="220" height="150"  />
-</a>
-</div></div>
-<div class="rental-title-produk"><a > Private Trip 2</div>
-<div class="widget biz_hours-widget list">
-                <ul class="unstyled">
+    <div class="col-md-4"><div class="rental-produk"> 
+    <div class="rental-gambar-center">
+    <div class="rental-gambar"><a><img src="img/wer.jfif"  width="220" height="150"  />
+    </a>
+    </div></div>
+    <div class="rental-title-produk"><a > Private Trip 2</div>
+    <div class="widget biz_hours-widget list">
+            <ul class="unstyled">
         								
-                <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 1.700.000</strong></span></li>
-             
- 						
-             <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
-         
-             <li><span>Fasilitas :</span> <span class="right">Dokumentasi </strong></span></li>
-                     
-                     
-             <li><span>Lokasi Jemput :</span> <span class="right">Malang</span></li> 
-             
-                   
-                       
-             <li><span>Kapasitas :</span> <span class="right">Maks 6 orang</span></li>
-          
- 
-            
+            <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 1.700.000</strong></span></li>		
+            <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
+            <li><span>Fasilitas :</span> <span class="right">Dokumentasi </strong></span></li>       
+            <li><span>Lokasi Jemput :</span> <span class="right">Malang</span></li>          
+            <li><span>Kapasitas :</span> <span class="right">Maks 6 orang</span></li>
             </ul>
     </div>
 
-<div class="tombol">
-<a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
-</div>
+    <div class="tombol">
+    <a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
+    </div>
 
-</div></div>
-<div class="col-md-4"><div class="rental-produk"> 
-<div class="rental-gambar-center">
-<div class="rental-gambar"><a ><img src="img/se.jfif"  width="220" height="150"  />
-</a>
-</div></div>
-<div class="rental-title-produk"><a >Bromo Trip 1 </a></div>
-<div class="widget biz_hours-widget list">
-                <ul class="unstyled">
+    </div></div>
+    <div class="col-md-4"><div class="rental-produk"> 
+    <div class="rental-gambar-center">
+    <div class="rental-gambar"><a ><img src="img/se.jfif"  width="220" height="150"  />
+    </a>
+    </div></div>
+    <div class="rental-title-produk"><a >Bromo Trip 1 </a></div>
+    <div class="widget biz_hours-widget list">
+            <ul class="unstyled">
         								
-                <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 350.000</strong></span></li>
-             
- 						
-             <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
-         
-             <li><span>Fasilitas :</span> <span class="right">- </strong></span></li>
-                     
-                     
-             <li><span>Lokasi Jemput :</span> <span class="right">Malang</span></li> 
-             
-                   
-                       
-             <li><span>Kapasitas :</span> <span class="right">1 orang</span></li>
-            
+            <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 350.000</strong></span></li>			
+            <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
+            <li><span>Fasilitas :</span> <span class="right">- </strong></span></li>       
+            <li><span>Lokasi Jemput :</span> <span class="right">Malang</span></li>           
+            <li><span>Kapasitas :</span> <span class="right">1 orang</span></li>
             </ul>
     </div>
 
-<div class="tombol">
-<a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
-</div>
+    <div class="tombol">
+    <a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
+    </div>
 
-</div></div>
-<div class="col-md-4"><div class="rental-produk"> 
-<div class="rental-gambar-center">
-<div class="rental-gambar"><a ><img src="img/er.jfif"  width="220" height="138"  />
-</a>
-</div></div>
-<div class="rental-title-produk"><a >Bromo Trip 2</a></div>
-<div class="widget biz_hours-widget list">
-                <ul class="unstyled">
+    </div></div>
+    <div class="col-md-4"><div class="rental-produk"> 
+    <div class="rental-gambar-center">
+    <div class="rental-gambar"><a ><img src="img/er.jfif"  width="220" height="138"  />
+    </a>
+    </div></div>
+    <div class="rental-title-produk"><a >Bromo Trip 2</a></div>
+    <div class="widget biz_hours-widget list">
+            <ul class="unstyled">
         								
-                <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 400.000</strong></span></li>
-             
- 						
-             <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
-         
-             <li><span>Fasilitas :</span> <span class="right">- </strong></span></li>
-                     
-                     
-             <li><span>Lokasi Jemput :</span> <span class="right">Batu</span></li> 
-             
-                   
-                       
-             <li><span>Kapasitas :</span> <span class="right">1 orang</span></li
- 
-            
+            <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 400.000</strong></span></li>                            
+            <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
+            <li><span>Fasilitas :</span> <span class="right">- </strong></span></li>
+            <li><span>Lokasi Jemput :</span> <span class="right">Batu</span></li> 
+            <li><span>Kapasitas :</span> <span class="right">1 orang</span></li>
+
             </ul>
     </div>
 
-<div class="tombol">
-<a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
-</div>
+    <div class="tombol">
+    <a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
+    </div>
 
-</div></div>
-<div class="col-md-4"><div class="rental-produk"> 
-<div class="rental-gambar-center">
-<div class="rental-gambar"><a><img src="img/sd.jfif"  width="220" height="138""  />
-</a>
-</div></div>
-<div class="rental-title-produk"><a >Bromo Trip</a></div>
-<div class="widget biz_hours-widget list">
-                <ul class="unstyled">
+    </div></div>
+    <div class="col-md-4"><div class="rental-produk"> 
+    <div class="rental-gambar-center">
+    <div class="rental-gambar"><a><img src="img/sd.jfif"  width="220" height="138""  />
+    </a>
+    </div></div>
+    <div class="rental-title-produk"><a >Bromo Trip</a></div>
+    <div class="widget biz_hours-widget list">
+            <ul class="unstyled">
         								
-                <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 500.000</strong></span></li>
-             
- 						
-             <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
-         
-             <li><span>Fasilitas :</span> <span class="right">- </strong></span></li>
-                     
-                     
-             <li><span>Lokasi Jemput :</span> <span class="right">Surabaya</span></li> 
-             
-                   
-                       
-             <li><span>Kapasitas :</span> <span class="right">1 orang</span></li
- 
-            
+            <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 500.000</strong></span></li>			
+            <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
+            <li><span>Fasilitas :</span> <span class="right">- </strong></span></li>        
+            <li><span>Lokasi Jemput :</span> <span class="right">Surabaya</span></li>          
+            <li><span>Kapasitas :</span> <span class="right">1 orang</span></li>
+
             </ul>
     </div>
 
-<div class="tombol">
-<a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
-</div>
+    <div class="tombol">
+    <a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
+    </div>
 
-</div></div>
-<div class="col-md-4"><div class="rental-produk"> 
-<div class="rental-gambar-center">
-<div class="rental-gambar"><a ><img src="img/y.jfif"  width="220" height="138"  />
-</a>
-</div></div>
-<div class="rental-title-produk"><a >Full Trip</a></div>
-<div class="widget biz_hours-widget list">
-                <ul class="unstyled">
+    </div></div>
+    <div class="col-md-4"><div class="rental-produk"> 
+    <div class="rental-gambar-center">
+    <div class="rental-gambar"><a ><img src="img/y.jfif"  width="220" height="138"  />
+    </a>
+    </div></div>
+    <div class="rental-title-produk"><a >Full Trip</a></div>
+    <div class="widget biz_hours-widget list">
+            <ul class="unstyled">
         								
-                <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 2.000.000</strong></span></li>
-             
- 						
-             <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>
-         
-             <li><span>Fasilitas :</span> <span class="right">Full Fasilitas </strong></span></li>
-                     
-                     
-             <li><span>Lokasi Jemput :</span> <span class="right">Malang</span></li> 
-             
-                   
-                       
-             <li><span>Kapasitas :</span> <span class="right">6 orang</span></li
- 
-            
+            <li><span>Harga :</span> <span class="right"><strong class="hargamobil">Rp 2.000.000</strong></span></li>	
+            <li><span>Destinasi :</span> <span class="right">All Destinasi</span></li>       
+            <li><span>Fasilitas :</span> <span class="right">Full Fasilitas </strong></span></li>                 
+            <li><span>Lokasi Jemput :</span> <span class="right">Malang</span></li>         
+            <li><span>Kapasitas :</span> <span class="right">6 orang</span></li>
+
             </ul>
     </div>
 
-<div class="tombol">
-<a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
+    <div class="tombol">
+    <a href="https://api.whatsapp.com/send?phone=+6289601506336&text=Halo"> <font color="red">Pesan Sekarang</font></button></a>
+    </div>
+    </ol>
+    </div>
 </div>
-                </ol>
-            </div>
-        </div>
 
-
-
-
-
-
-
-
-
-
-
- 
 @endsection
